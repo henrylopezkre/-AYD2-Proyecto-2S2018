a7a4217ec72a7b78da16f958df969c8bdda5fc43
Henry López <usuario@ayd2.com>
2018-11-02 12:19:55 -0600

    Se modificó el archivo: tests/Feature/AppFeatureTest.php

--- tests/Feature/AppFeatureTest.php
+++ tests/Feature/AppFeatureTest.php
@@ -30,7 +30,7 @@ class AppFeatureTest extends TestCase
             'age' => 18,
             'gender' => true,
             'photo' => "",
-            'comment' => "Comentario"
+            'comment' => "Comentari"
             ];
 
         $this->call('POST', 'user/update', $data)
